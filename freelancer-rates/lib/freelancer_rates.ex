d262a5bbac5ffb80b388de93e4b8f42d6aec61b4
Fahrul Fauzi <fahrul.fauz@gmail.com>
2023-01-16 19:34:24 +0700

    freelance rates :)

--- freelancer-rates/lib/freelancer_rates.ex
+++ freelancer-rates/lib/freelancer_rates.ex
@@ -1,17 +1,12 @@
 defmodule FreelancerRates do
-  def daily_rate(hourly_rate) do
-    # Please implement the daily_rate/1 function
-  end
+  @daily 8.0
+  @bill_dates 22.0
 
-  def apply_discount(before_discount, discount) do
-    # Please implement the apply_discount/2 function
-  end
+  def daily_rate(hourly_rate), do: hourly_rate * @daily
 
-  def monthly_rate(hourly_rate, discount) do
-    # Please implement the monthly_rate/2 function
-  end
+  def apply_discount(before_discount, discount), do: before_discount - (discount / 100.0 * before_discount)
 
-  def days_in_budget(budget, hourly_rate, discount) do
-    # Please implement the days_in_budget/3 function
-  end
+  def monthly_rate(hourly_rate, discount), do: trunc(Float.ceil((hourly_rate * @daily * @bill_dates) - ((discount / 100.0) * (hourly_rate * @daily * @bill_dates)), 0))
+
+  def days_in_budget(budget, hourly_rate, discount), do: Float.floor(((discount / 100 * budget + 1) + budget) / (hourly_rate * @daily), 1)
 end
